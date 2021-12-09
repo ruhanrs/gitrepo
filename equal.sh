26bf3e45af4f151ced9e44de4f05362290123954
ruhanrs <mahamadruhans@gmail.com>
2021-12-09 12:14:44 +0000

    testing

---
+++ equal.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Enter the number"
+read Num
+num1=5
+if [ $Num -eq $num1 ]
+then 
+	echo "Give no is $Num "
+else 
+	echo "Given no is not 5 "
+fi
---
+++ fact.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "enter the value N "
+read num
+mul=1
+while [  $num -gt 0 ]
+do
+	mul=`expr $mul \* $num`
+	num=`expr $num - 1`
+done
+echo "sum of first N number is $mul "
+
+
+
